7af426e6c6a2f39f169ebc4726d867bad8d3f9ad
GlebMoskalev <gleb12122003@gmail.com>
2024-10-20 16:40:13 +0500

    lesson 4

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 
-struct PageView: View {
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+                .aspectRatio(3 / 2, contentMode: .fit)
+            PageControl(numberPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
     }
 }
 
 #Preview {
-    PageView()
+    PageView(pages: ModelData().features.map{ FeatureCard(landmark: $0) })
 }
