f46931474076cbe1b8e60bed839f7ac809203325
GlebMoskalev <gleb12122003@gmail.com>
2024-10-20 12:27:24 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Глеб Москалев on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
